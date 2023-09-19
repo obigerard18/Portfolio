c4da95d0333eb935177521823864ef3561a8a738
Gerard C. Obi <92319626+obigerard18@users.noreply.github.com>
2023-09-19 12:36:16 +0100

    Junies cars

---
+++ Car_Rental_Website/Junies_Cars/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JuniesCarsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Junies_Cars'
--- Car_Rental_Website/Junies_Cars/views.py
+++ Car_Rental_Website/Junies_Cars/views.py
@@ -1,1 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html',{})
+
+def about(request):
+    return render(request, 'about.html',{})
+
+def booking(request):
+    return render(request, 'booking.html',{})
+
+def login(request):
+    return render(request, 'login.html',{})
+
+def signup(request):
+    return render(request, 'signup.html',{})
+
+def contact(request):
+    return render(request, 'contact.html',{})
 
